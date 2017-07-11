4d3842e2592763a8670d2634a2fd8d2d4c85f8ae
Damian Galiniewicz <gnwm84@motorolasolutions.com>
2017-07-11 15:48:00 +0200

    Signup is working now

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,17 @@ router.get('/', function (req, res, next) {
     // res.render('shop/index', {title: 'Shopping Cart', products: someArray});
 
     Product.find(function (err, callback) {
-        res.render('shop/index', {title: 'Shopping Cart', products: callback}); // does not working !!!! Powtorz odcinek 5
+        console.log('Callback ' + callback);
+        const productChunks = [];
+        const chunkSize = 3;
+        for (let i = 0; i < callback.length; i += chunkSize) {
+            productChunks.push(callback.slice(i, i + chunkSize ));
+        }
+        console.log('Getting products from mongodb.');
+        if (productChunks.length > 0 ) {
+            console.log('Products ' + productChunks);
+        }
+        res.render('shop/index', {title: 'Shopping Cart', products: productChunks}); // does not working !!!! Powtorz odcinek 5
     });
 
 });
@@ -27,8 +37,6 @@ router.get('/user/signup', function (req, res, next) {
 });
 
 router.post('/user/signup', passport.authenticate('local.signup', {
-    // does not working, check why redirects to users/users/signup
-    // instead of users/profile (odc.7, 20.00)
     successRedirect: '/user/profile',
     failureRedirect: '/user/signup',
     failureFlash: true // flash a message to true
